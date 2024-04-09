cdf5c54e594aa5f38e4a3717034bef799e1970f6
Darren Tu <darrentu@Darrens-MacBook-Pro.local>
2024-04-08 21:09:53 -0700

    update to catch error

--- background.js
+++ background.js
@@ -1,13 +1,16 @@
 const msgForChange = "changed"; // message for when there is a change in the url
 
 chrome.tabs.onUpdated.addListener(
-    (tabId, tabChanged) => {
-      if (tabChanged.url) {
-        chrome.tabs.sendMessage( tabId, {
-          message: msgForChange,
-          url: tabChanged.url
-        })
-      }
+  (tabId, changeInfo) => {
+    if (changeInfo.url && changeInfo.url.includes("https://www.linkedin.com")) {
+
+      // Use sendMessage with a callback to handle potential errors gracefully.
+      chrome.tabs.sendMessage(tabId, { message: msgForChange, url: changeInfo.url }, (response) => {
+        if (chrome.runtime.lastError) {
+          // Handle the error gracefully. For example, log it or take alternative actions.
+          console.log('Error sending message:', chrome.runtime.lastError.message);
+        }
+      });
     }
-    
+  }
 );
